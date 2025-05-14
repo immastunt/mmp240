5936d237d5e2d27d89126c19bbc94881d3be18cd
immastunt <nickypet0215@gmail.com>
2025-05-14 15:13:51 +0000

    finishingup

--- CUNYdesign/showMenu.js
+++ CUNYdesign/showMenu.js
@@ -2,6 +2,7 @@ function showMenu(){
     var  menu = document.getElementById("top-nav");
     if  (menu.style.display==="none" || menu.style.display === ''){
         menu.style.display="block";
+        menu.style.transform="translateY(100px)"
     } else {
     menu.style.display="none";
         }
